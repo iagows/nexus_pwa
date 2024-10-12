b504dff64227934ffc8298abd6e78e709093cb7b
Iago <el.iagows@gmail.com>
2024-10-12 15:53:55 -0300

    feat: pwa

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+	onNeedRefresh() {
+		if (confirm("Novo conteúdo disponível. Recarregar?")) {
+			updateSW(true);
+		}
+	},
+});
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById("root")!).render(
