caabe4e65809279bda69044fecdd5ef0b8689de3
Iago Alves <el.iagows@gmail.com>
2024-12-14 10:48:50 -0300

    feat: bg

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,9 @@
 import { Box, type SxProps, type Theme } from "@mui/material";
+import { useLocation } from "react-router-dom";
 import empty from "../assets/images/empty.jpg";
 import nonEmpty from "../assets/images/non-empty.jpg";
 import AppMainLayout from "../navigation/AppMainLayout";
+import RouteNames from "../navigation/RouteNames";
 import useFichaStore from "../stores/slices/ficha/useFichaStore";
 
 const bgCss: (isEmpty: boolean) => SxProps<Theme> = (isEmpty) => ({
@@ -15,14 +17,20 @@ const bgCss: (isEmpty: boolean) => SxProps<Theme> = (isEmpty) => ({
 	justifyContent: "center",
 });
 
+const useMain = () => {
+	const location = useLocation();
+	return location.pathname === RouteNames.FICHAS;
+};
+
 const MainPage = () => {
+	const isPageFichas = useMain();
 	const { list } = useFichaStore();
 
 	return (
 		<Box
 			display={"flex"}
 			flexDirection={"column"}
-			sx={bgCss(list.length === 0)}
+			sx={isPageFichas ? bgCss(list.length === 0) : null}
 		>
 			<Box display={"flex"}>
 				<AppMainLayout />
